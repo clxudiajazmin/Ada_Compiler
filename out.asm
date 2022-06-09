f4d998d4f062eb5ec6b6c8cf54d0b0c8d0ee84fb
Claudia Jazmín Soria Saavedra <clxudiajazmin@gmail.com>
2022-06-09 22:34:23 +0200

    Prueba

--- out.asm
+++ out.asm
@@ -1,13 +1,19 @@
 .data
 # variables
-a: .byte 'c'
-b: .float 2.500000
-c: .float 7.200000
+A: .word 6
+B: .float 4.500000
+C: .word 9
+D: .float 5.600000
+letra: .byte 'e'
 # messages
+msg1: .asciiz "Compiladores"
 
 .text
 main:
-lwc1 $f0, c
-lwc1 $f0, b
+LW $s0, A($0)
+ADDI $s0, $s0, 2
+MOVE $s0, $s0
+lwc1 $f0, B
+lwc1 $f0, D
 add.s $f2, $f0 $f0 
 mov.s $f0, $f2
--- pruebas/mips.ada
+++ pruebas/mips.ada
@@ -3,6 +3,8 @@ procedure Main is
    B    : Float:= 4.5;
    C    : Integer := 9;
    D    : Float := 5.6;
+   asignatura : String := "Compiladores";
+   letra : Character := 'e';
 begin
    A := A + 2;
    B := B + D;
